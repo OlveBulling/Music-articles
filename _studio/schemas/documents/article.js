8c0d5f40ae83310bde50de19c044a685c2179ff5
Olve Bulling Johansen <112381026+OlveBulling@users.noreply.github.com>
2023-05-08 14:32:08 +0200

    Sanity document article (#10)
    * created article.js
    
    * Added slug, image and author field to article
    
    * added fields: Lead, date, text_content, category and related articles

---
+++ _studio/schemas/documents/article.js
@@ -0,0 +1,65 @@
+export default {
+	title: 'Article',
+	name: 'article',
+	type: 'document',
+	fields: [
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'string',
+			validation: (Rule) => Rule.required()
+		},
+		{
+			title: 'Slug',
+			name: 'slug',
+			type: 'slug',
+			options: {
+				source: 'title'
+			},
+			validation: (Rule) => Rule.required()
+		},
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'image',
+		},
+		{
+			title: 'Author',
+			name: 'author',
+			type: 'reference',
+			to: { type: 'author' },
+			validation: (Rule) => Rule.required()
+		},
+		{
+			title: 'Lead',
+			name: 'lead',
+			type: 'string'
+		},
+		{
+			title: 'Date',
+			name: 'date',
+			type: 'date',
+			description: 'The date the article will be published',
+		},
+		{
+			title: 'Text content',
+			name: 'text_content',
+			type: 'array',
+			of: [{ type: 'block' }],
+			validation: (Rule) => Rule.required()
+		},
+		{
+			title: 'Category',
+			name: 'category',
+			type: 'reference',
+			to: { type: 'category' },
+			validation: (Rule) => Rule.required()
+		},
+		{
+			title: 'Related articles',
+			name: 'related_articles',
+			type: 'reference',
+			to: { type: 'article' }
+		}
+	]
+}
