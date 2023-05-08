81c091b7b642122fb0037fb6da0026f40163e50b
Olve <olvejohansen@gmail.com>
2023-05-08 15:02:48 +0200

    Created category document

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,7 +1,9 @@
 import article from "./documents/article.js"
 import author from "./documents/author.js"
+import category from "./documents/category.js"
 
 export default [
 	article,
 	author,
+	category,
 ]
