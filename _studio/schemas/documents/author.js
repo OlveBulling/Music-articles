9834a9e22884dc03a28b9daa3ed3a1e624e87798
Olve <olvejohansen@gmail.com>
2023-05-08 14:40:21 +0200

    added name field to author.js

---
+++ _studio/schemas/documents/author.js
@@ -0,0 +1,13 @@
+export default {
+	title: 'Author',
+	name: 'author',
+	type: 'document',
+	fields: [
+		{
+			title: 'Name',
+			name: 'name',
+			type: 'string',
+			description: 'The authors FULL NAME',
+		}
+	]
+}
