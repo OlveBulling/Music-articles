a3a7b5830618614c892f333d0c227b3fc0ea55d4
Olve <olvejohansen@gmail.com>
2023-05-11 15:28:08 +0200

    added category to article cards

--- _app/js/modules/generate-article-cards.js
+++ _app/js/modules/generate-article-cards.js
@@ -2,18 +2,23 @@ export default function generateArticleCards(article) {
 	const articleCard = document.createElement("div");
 	const articleImage = document.createElement("img");
 	const articleTitle = document.createElement("h2");
+	const articleCategory = document.createElement("h3");
 
 	articleCard.className = "article__card";
 	articleImage.className = "article__card-image";
 	articleTitle.className = "article__card-title";
+	articleCategory.className = "article__card-category";
 
 	articleImage.src = article.image;
 	articleImage.alt = article.image_alt_text;
 
 	articleTitle.textContent = article.title;
 
+	articleCategory.textContent = article.category;
+
 	articleCard.appendChild(articleImage);
 	articleCard.appendChild(articleTitle);
+	articleCard.appendChild(articleCategory);
 
 	return articleCard;
 }
