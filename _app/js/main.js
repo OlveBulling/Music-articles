bccd3a19a6a7f0b741f5110360af872e6c3c0447
Olve <olvejohansen@gmail.com>
2023-05-11 13:01:42 +0200

    generated button for each category

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,3 +1,5 @@
 import { renderArticleCards } from "./modules/render-article-cards.js";
+import { generateCategoryButtons } from "./modules/generate-category-buttons.js";
 
+generateCategoryButtons();
 renderArticleCards();
--- _app/js/modules/generate-category-buttons.js
+++ _app/js/modules/generate-category-buttons.js
@@ -1,5 +1,18 @@
-import { getAllArticles } from "./get-articles";
+import { getAllArticles } from "./get-articles.js";
 
-export default async function generateCategoryButtons() {
-	
+export async function generateCategoryButtons() {
+	const articles = await getAllArticles();
+
+	const buttonContainer = document.getElementById('menu__category-buttons-container');
+
+	articles.forEach((article, index) => {
+		const categories = articles[index].category;
+		const categoryButton = document.createElement('button');
+
+		categoryButton.className = 'menu__category-button'
+		categoryButton.innerText = categories;
+
+		buttonContainer.appendChild(categoryButton);
+
+	})
 }
