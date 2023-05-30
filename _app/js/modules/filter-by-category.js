342720c44b2d5a6372c9b228eb07d4cbf12e6896
Olve Bulling Johansen <112381026+OlveBulling@users.noreply.github.com>
2023-05-30 15:49:18 +0200

    Comments (#30)
    * corrected menu class name
    
    * organised fonts
    
    * added comments and corrected class name
    
    * added comments
    
    * commented code
    
    * fixed formating

--- _app/js/modules/filter-by-category.js
+++ _app/js/modules/filter-by-category.js
@@ -1,11 +1,12 @@
 export function filterArticlesByCategory(category) {
 	const articleCards = document.querySelectorAll('.article__card');
 
+	// hides articles without chosen category using css
 	articleCards.forEach((card) => {
 		if (card.querySelector('.article__card-category').textContent === category) {
-			card.classList.remove('hidden-article');
+			card.classList.remove('article__card--hidden');
 		} else {
-			card.classList.add('hidden-article');
+			card.classList.add('article__card--hidden');
 		}
 	});
 }
